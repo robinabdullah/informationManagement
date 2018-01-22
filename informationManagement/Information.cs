ff39cdb232de17d7ced2efaaa7d346548c1f2022
robinabdullah <robinabdullah@yahoo.com>
2018-01-22 23:59:53 +0600

    updated

--- informationManagement/Information.cs
+++ informationManagement/Information.cs
@@ -7,7 +7,8 @@ namespace informationManagement
 {
     public class Information
     {
-        public static String connectionstring = @"Server=tcp:posnew12.database.windows.net,1433;Initial Catalog=POSNew12;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
+        public static String connectionstring = @"Server=tcp:posnew12.database.windows.net,1433;Initial Catalog=POSNew12;Persist Security Info=False;User ID=robinabdullah;Password=45659123iN;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        //public static String connectionstring = @"server=DESKTOP-JAP561I\SQLEXPRESS;database=attendenceManagement;integrated security=true";
+        //public static String connectionstring = @"server=Robin\SQLEXPRESS;database=attendenceManagement;integrated security=true";
     }
 }
