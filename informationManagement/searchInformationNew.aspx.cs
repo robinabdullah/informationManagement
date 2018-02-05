dc0a71dfa014a8718fc95645e69156980643a64c
robinabdullah <robinabdullah@yahoo.com>
2018-02-06 01:26:37 +0600

    order by sorting in search page

--- informationManagement/searchInformationNew.aspx.cs
+++ informationManagement/searchInformationNew.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -28,7 +29,8 @@ namespace informationManagement
             list.DataSource = null;
             list.DataBind();
             String search;
-            string commonQuery = "select Information.id, Information.Name, Class, Section, Department, Gender, Roll, Shift, Title,Office_Phone as Personal_Number, DateofBirth, Mobile_Number as Guardian_Number,Home_Address, newLogin.name as Created_By,Created_Date, Updated_By,Updated_Date, Image_Provided, Form_Filled, Blood_Group, Blood_Group_Checked, Is_Paid as Paid, Deleted_By, Deleted_Date from Information, newLogin where created_by=newLogin.ID ";
+            //string commonQuery = "select Information.id, Information.Name, Class, Section, Department, Gender, Roll, Shift, Title,Office_Phone as Personal_Number, DateofBirth, Mobile_Number as Guardian_Number,Home_Address, newLogin.name as Created_By,Created_Date, Updated_By,Updated_Date, Image_Provided, Form_Filled, Blood_Group, Blood_Group_Checked, Is_Paid as Paid, Deleted_By, Deleted_Date from Information, newLogin where created_by=newLogin.ID ";
+            string commonQuery = "select Information.id, Information.Name, Class, Section, Department, Gender, Cast(Roll as int) as Roll, Shift, Title,Office_Phone as Personal_No, DateofBirth as DOB, Mobile_Number as Guardian_No,Home_Address, newLogin.name as 'Created_By',Created_Date, Updated_By,Updated_Date, (Case When Image_Provided = 1 then 'Yes' Else '' End) As 'Image Provided', (Case When Form_Filled = 1 then 'Yes' Else '' End) As 'Form Filled' , Blood_Group, (Case When Blood_Group_Checked = 1 then 'Yes' Else '' End) As 'Blood Group Checked' , (Case When Is_Paid = 1 then 'Yes' Else '' End) As 'Paid' , Deleted_By, Deleted_Date from Information, newLogin where created_by = newLogin.ID";
 
             string countQuery = "select count(id) as count from information where 1 = 1 ";
 
@@ -42,16 +44,20 @@ namespace informationManagement
                 string selectedMobileNo = mobile.Text == "" ? "" : " and Mobile_Number='" + mobile.Text + "'";
                 string selectedClass = clas.SelectedIndex == 0 ? "" : " and class='" + clas.SelectedItem.Text + "'";//class 9
                 string selectedShift = shift.SelectedIndex == 0 ? "" : " and shift='" + shift.SelectedItem.Text + "'";
+                string selectedGender = gender.SelectedIndex == 0 ? "" : " and gender='" + gender.SelectedItem.Text + "'";
+                string selectedDepartment = department.SelectedIndex == 0 ? "" : " and department='" + department.SelectedItem.Text + "'";
                 string selectedTitle = title.SelectedIndex == 0 ? "" : " and title='" + title.SelectedItem.Text + "'";
                 string selectedBloodGroup = bloodGroup.SelectedIndex == 0 ? "" : " and Blood_Group='" + bloodGroup.SelectedItem.Text + "'";
                 string selectedBloodGroupChecked = bloodGroupChecked.SelectedIndex == 0 ? "" : " and Blood_Group != 'N/A' and Blood_Group_Checked=" + bloodGroupChecked.SelectedValue;
                 string selectedImageProvided = imageProvided.Checked == false ? "" : " and Image_Provided=1" ;
                 string selectedFormFilled = formFilled.Checked == false ? "" : " and Form_Filled=1" ;
                 string selectedPaymentType = paymentType.SelectedIndex == 0 ? "" : " and Is_paid=" + paymentType.SelectedValue;
-                if (withTableData.Checked == true)
-                    search = commonQuery + selectedOfficeMobile + selectedMobileNo + selectedClass + selectedShift + selectedTitle + selectedBloodGroup + selectedBloodGroupChecked + selectedPaymentType + selectedImageProvided + selectedFormFilled;
+
+                string orderByColumns = orderBy.SelectedIndex == 0 ? "" : " order by " + orderBy.SelectedValue;
+                if (withTableData.Checked == true || orderByColumns != "")
+                    search = commonQuery + selectedOfficeMobile + selectedMobileNo + selectedClass + selectedShift + selectedTitle+ selectedGender +selectedDepartment + selectedBloodGroup + selectedBloodGroupChecked + selectedPaymentType + selectedImageProvided + selectedFormFilled + orderByColumns;
                 else
-                    search = countQuery + selectedOfficeMobile + selectedMobileNo + selectedClass + selectedShift + selectedTitle + selectedBloodGroup + selectedBloodGroupChecked + selectedPaymentType + selectedImageProvided + selectedFormFilled;
+                    search = countQuery + selectedOfficeMobile + selectedMobileNo + selectedClass + selectedShift + selectedTitle + selectedGender + selectedDepartment + selectedBloodGroup + selectedBloodGroupChecked + selectedPaymentType + selectedImageProvided + selectedFormFilled + orderByColumns;
 
             }
             
@@ -59,18 +65,20 @@ namespace informationManagement
             SqlCommand cmd = new SqlCommand(search, conn);
             conn.Open();
             SqlDataReader reader = null;
-            if (withTableData.Checked == true || id.Text.Trim() != "")
+            if (withTableData.Checked == true || id.Text.Trim() != "" || orderBy.SelectedIndex != 0)
             {
                 reader = cmd.ExecuteReader();
                 list.DataSource = reader;
                 list.DataBind();
                 msg.Text = list.Rows.Count + " records found";
                 reader.Close();
+                download.Enabled = true;
             }
             else
             {
                 int rows = (int)cmd.ExecuteScalar();
                 msg.Text = rows + " records found";
+                download.Enabled = true;
             }
 
             withTableData.Checked = false;
@@ -104,6 +112,8 @@ namespace informationManagement
             clas.SelectedIndex = 0;
 
             shift.SelectedIndex = 0;
+            department.SelectedIndex = 0;
+            gender.SelectedIndex = 0;
 
             title.SelectedIndex = 0;
 
@@ -120,9 +130,37 @@ namespace informationManagement
             withTableData.Checked = false;
             list.DataSource = null;
             list.DataBind();
-
+            download.Enabled = false;
+            orderBy.SelectedIndex = 0;
             msg.Text = "";
         }
-
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            /* Confirms that an HtmlForm control is rendered for the specified ASP.NET
+           server control at run time. */
+            //EnableEventValidation = false;
+        }
+        protected void download_Click(object sender, EventArgs e)
+        {
+            if (list.Rows.Count > 0)
+            {
+                Response.Clear();
+                Response.Buffer = true;
+                Response.ClearContent();
+                Response.ClearHeaders();
+                Response.Charset = "";
+                string FileName = clas.SelectedItem.Text + " " + shift.SelectedItem.Text + " " + DateTime.Now + ".xls";
+                StringWriter strwritter = new StringWriter();
+                HtmlTextWriter htmltextwrtter = new HtmlTextWriter(strwritter);
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.ContentType = "application/vnd.ms-excel";
+                Response.AddHeader("Content-Disposition", "attachment;filename=" + FileName);
+                list.GridLines = GridLines.Both;
+                list.HeaderStyle.Font.Bold = true;
+                list.RenderControl(htmltextwrtter);
+                Response.Write(strwritter.ToString());
+                Response.End();
+            }
+        }
     }
 }
